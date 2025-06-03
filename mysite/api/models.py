f5bc1edae9f99fed15165a9b39c3b23f3eb8e0a4
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-06-03 13:00:07 +0500

    added ExchangeRate

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class ExchangeRate(models.Model):
+    """Сущность:Курс обмена"""
+    rate = models.DecimalField(verbose_name="Price", max_digits=8, decimal_places=4)
+    timestamp = models.DateTimeField(auto_now_add=True)
